ede676d6649619a3d6216f42d550f6066d4c40a1
gaoli <gaoli360681@gmail.com>
2026-03-17 20:16:19 +0800

    更新虚拟环境

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\虚拟教育\虚拟课堂\前端\.venv
+set VIRTUAL_ENV=D:\虚拟教育\虚拟课堂\CyberClassroom-Streamlit\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
